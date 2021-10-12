9fc9a317982d1df68000e5599fbd9d454441f77d
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2021-10-12 10:20:49 -0500

    Update build.

--- src/App.js
+++ src/App.js
@@ -5,22 +5,20 @@ import useLoader from './useLoader'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import iphoneModelPath from '../assets/iphone.glb'
 
-// const REMOTE_FILE_URL = 'https://raw.githack.com/CodyJasonBennett/r3f-native-starter/main/assets/iphone.glb'
-
-const Model = (props) => {
-  const { scene } = useLoader(GLTFLoader, iphoneModelPath)
-  return <primitive {...props} object={scene} />
+const Model = ({ url, ...rest }) => {
+  const { scene } = useLoader(GLTFLoader, url)
+  return <primitive {...rest} object={scene} />
 }
 
 const App = () => (
-  <Canvas camera={{ position: [-6, 0, 16], fov: 36 }} onCreated={({ gl }) => (gl.physicallyCorrectLights = true)}>
+  <Canvas gl={{ physicallyCorrectLights: true }} camera={{ position: [-6, 0, 16], fov: 36 }}>
     <color attach="background" args={[0xe2f4df]} />
     <ambientLight />
     <directionalLight intensity={1.1} position={[0.5, 0, 0.866]} />
     <directionalLight intensity={0.8} position={[-6, 2, 2]} />
     <Suspense fallback={null}>
       <Environment preset="park" />
-      <Model />
+      <Model url={iphoneModelPath} />
     </Suspense>
   </Canvas>
 )
