dcd36a4451fd8eb5b38d79c50ef42f056a424703
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2021-10-13 12:07:13 -0500

    Create RGBELoader test case.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React, { Suspense, useEffect } from 'react'
-import { TextureLoader, sRGBEncoding, MeshBasicMaterial } from 'three'
+import React, { useMemo, useEffect, Suspense } from 'react'
+import * as THREE from 'three'
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { useThree, Canvas } from '@react-three/fiber'
-import { Environment } from '@react-three/drei/core/Environment'
+import { presetsObj } from '@react-three/drei/helpers/environment-assets'
 import useLoader from './useLoader'
 import iphoneModelPath from '../assets/iphone.glb'
 import screenTexturePath from '../assets/screen-texture.jpg'
 
+const CUBEMAP_ROOT = 'https://rawcdn.githack.com/pmndrs/drei-assets/aa3600359ba664d546d05821bcbca42013587df2'
+
+const Environment = ({ background = false, preset }) => {
+  const { gl, scene } = useThree()
+  const data = useLoader(RGBELoader, `${CUBEMAP_ROOT}/hdri/${presetsObj[preset]}`)
+  const texture = useMemo(() => {
+    const map = new THREE.DataTexture(data.data, data.width, data.height)
+    const gen = new THREE.PMREMGenerator(gl)
+    const texture = gen.fromEquirectangular(map).texture
+    gen.dispose()
+
+    return texture
+  }, [])
+
+  useEffect(() => {
+    scene.environment = texture
+    if (background) scene.background = texture
+  }, [texture, background])
+
+  return null
+}
+
 const Model = ({ url, screenUrl, ...rest }) => {
   const { gl } = useThree()
   const { scene } = useLoader(GLTFLoader, url)
-  const texture = useLoader(TextureLoader, screenUrl)
+  const texture = useLoader(THREE.TextureLoader, screenUrl)
 
   useEffect(() => {
     scene.traverse((node) => {
       if (node.name === 'Screen') {
-        texture.encoding = sRGBEncoding
+        texture.encoding = THREE.sRGBEncoding
         texture.flipY = false
         texture.anisotropy = gl.capabilities.getMaxAnisotropy()
-        node.material = new MeshBasicMaterial({ map: texture })
+        node.material = new THREE.MeshBasicMaterial({ map: texture })
       }
     })
   }, [texture])
@@ -33,7 +56,7 @@ const App = () => (
     <directionalLight intensity={1.1} position={[0.5, 0, 0.866]} />
     <directionalLight intensity={0.8} position={[-6, 2, 2]} />
     <Suspense fallback={null}>
-      <Environment preset="park" />
+      <Environment background preset="park" />
       <Model url={iphoneModelPath} screenUrl={screenTexturePath} />
     </Suspense>
   </Canvas>
